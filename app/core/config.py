0bebcbe960bffc80b87a9cc7ea5a73223a5a532c
Fhil Joshua <husphil200@gmail.com>
2026-09-19 17:23:52 +0800

    Remove COMICK_COOKIE from .env.example and update config.py to streamline environment variable management

--- app/core/config.py
+++ app/core/config.py
@@ -2,16 +2,15 @@ from pydantic_settings import BaseSettings
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-class Settings(BaseSettings):
-    COMICK_COOKIE: str = os.getenv("COMICK_COOKIE")
 
+class Settings(BaseSettings):
+    DEV_ENVIRONMENT: bool = os.getenv("DEV_ENVIRONMENT", True) == "true"
 
     class Config:
         extra = "allow"
         env_file = ".env"  # Load environment variables
-        
+
 
 settings = Settings()
