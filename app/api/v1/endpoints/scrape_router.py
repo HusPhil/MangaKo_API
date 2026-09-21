22f80d212486e9ad8b2da8a277eb6117f4e47d50
Fhil Joshua <husphil200@gmail.com>
2026-09-21 16:59:18 +0800

    Remove unused test scrape route from scrape_router.py

--- app/api/v1/endpoints/scrape_router.py
+++ app/api/v1/endpoints/scrape_router.py
@@ -9,31 +9,6 @@ import hashlib, httpx, json
 router = APIRouter()
 
 
-@router.get("/")
-async def test_scrape():
-
-    acc_id = "REDACTED"
-    api_key = "REDACTED"
-
-    # return {'message': 'scrape route working!', 'supported_sources': supported_sources}
-    base_url = (
-        "https://api.cloudflare.com/client/v4/accounts/"
-        + acc_id
-        + "/browser-rendering/content"
-    )
-    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
-
-    payload = {"url": "https://fto.to/apo/"}
-
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(base_url, headers=headers, data=json.dumps(payload))
-        return {"message": "scrape route working!", "response": resp.json()}
-    return {
-        "message": "scrape route working!",
-    }
-    pass
-
-
 @router.get("/sources")
 def get_supported_sources() -> List[Source]:
     return list(SUPPORTED_SOURCES.values())
